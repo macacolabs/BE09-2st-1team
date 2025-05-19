5ef4ffcaa748514a0e9b783a9b203a6a673e66ac
seungje_yu <coffit23@gmail.com>
2025-05-19 15:44:24 +0900

    test

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,11 +2,10 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-export default config;
-
-module.exports = {
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
